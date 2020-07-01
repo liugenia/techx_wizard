da736d1c7f04b13895ab7166cd65d2c9abeb4a36
Eugenia Liu <eulyliu@gmail.com>
2020-07-01 00:32:33 -0700

    cleaned up code

--- meraki_GUI.py
+++ meraki_GUI.py
@@ -2,14 +2,15 @@ from tkinter import *
 from tkinter import filedialog as fd
 from meraki_GUI_APIcall import *
 import xlrd
-import numpy
 
 class MerakiWizard(Frame):
+
     ####MERAKI GUI INTERFACE####
     def __init__(self,parent,*args,**kwargs):
         self.parent=parent
         self.org_info=orgInfo()
-        #Select Organization label
+
+        ####PROMPTS USER TO SELECT ORGANIZATION####
         self.org_title=Label(text="Select Organization")
         self.org_title.grid(row=0,column=0)
         #Org Listbox: takes Org Name, passes Org ID on button click to get list of networks
@@ -18,21 +19,25 @@ class MerakiWizard(Frame):
         for org_name in self.getOrgList():
             self.orgMenu.insert(END,org_name)
             self.orgMenu.bind("<<ListboxSelect>>", self.popuNetNames)
-        #Select Network label
+
+        ####PROMPTS USER TO SELECT NETWORK####
         self.net_title=Label(text="Select Network")
         self.net_title.grid(row=0, column=1)
         #Network Listbox: takes Network Name, passes Network ID on button click to get list of devices
         self.netMenu=Listbox(root,exportselection=False)
         self.netMenu.grid(row=1, column=1)
         self.netMenu.bind("<<ListboxSelect>>", self.popuDevNames)
-        #Select Devices label
+
+        ####PROMPTS USER TO SELECT DEVICE####
         self.net_title=Label(text="Select Device")
         self.net_title.grid(row=0, column=2)
         #Device Listbox: Takes Device Name, passes Device 
         self.devMenu=Listbox(root,exportselection=False)
         self.devMenu.grid(row=1,column=2)
-        self.devMenu.bind("<<ListboxSelect>>", self.getNetDevSerials)
-        #network option menu
+        self.devMenu.bind("<<ListboxSelect>>", self.devInfoClick)
+
+        ####PROMPTS USER TO SELECT AN ACTION THEY WANT TO PERFORM####
+        #Alteration OptionMenu: list of things to do on (1)org (2)network (3)device level
         self.commands=commandList()
         self.alter=StringVar(root)
         self.alter.set(self.commands[0])
@@ -43,108 +48,103 @@ class MerakiWizard(Frame):
             root,text='Select Action',command=self.AlterMenu, bg='#049fd9', fg='white')
         self.selectButton.grid(row=4,column=1)
 
-    ####ALTERATION DROPDOWN MENU####
-    def AlterMenu(self):
+    ####ALTERATION OPTIONMENU####
+    def AlterMenu(self): #what to do depending on the option selected
         self.selected=self.alter.get()
         if self.selected=='Create Network':
             pass
         elif self.selected=='Delete Network': #deletes network and all devices
             deleteNetwork(self.getNetID())
-            self.popuNetNames()
         elif self.selected=='Add Device': #adds a single device to the network
             self.infoPopup()
-            self.popuDevNames()
-        elif self.selected=='Bulk Add Devices': #automatically adds all devices on .csv file
+        elif self.selected=='Bulk Add Devices': #automatically adds all devices on .xls file
             claimDevices(self.getNetID(),self.getDevicesExcel())
-            self.popuDevNames()
         elif self.selected=='Rename Device': #renames a device that is selected from listbox
             self.infoPopup()
-            self.popuDevNames()
+        ###BULK RENAME DEVICE in progress. takes device names, {show name}-{model}-(#) --> RSA-SW-1 ####
         elif self.selected=='Remove Device(s)': #removes the devices selected or all devices
             self.delDevices()
-        elif self.selected=='Bulk Add Address': #adds the input address for all devices
+        elif self.selected=='Bulk Add Address': #adds the trade show or event address to all devices
             self.infoPopup()
-        elif self.selected=='Add VLAN': #adds a VLAN given user input
+        elif self.selected=='Add VLAN': #adds a VLAN given user input of required info
             self.infoPopup()
+        ####BULK ADD DEFAULT VLANS in progress. Takes Vlan ID, Name, Subnet, Appliance IP from excel file and auto adds the vlans
         elif self.selected=='Delete VLAN': #deletes VLAN given the VLAN ID
             self.infoPopup()
         elif self.selected=='Swap MX Warm Spare': #switches primary and warm MX
             swapWarmSpare(self.getNetID()) 
         elif self.selected=='Blink LED': #blinks LED of chosen device
             blinkDevice(self.getDevSerial())
-        elif self.selected=='Update Device Port': #updates port type and vlan on chosen device
+        elif self.selected=='Update Device Port': #updates port type for specefied port (and vlan on chosen device if not trunk)
             self.infoPopup()
-    def infoPopup(self): #popup menu if needed for inputs required 
+    def infoPopup(self): #popup menu to get the user input of required parameters
         self.selected=self.alter.get()
         self.entry=Toplevel()
-        if self.selected=='Add Device':
-            self.getInput=Label(self.entry,text='Enter Device SN:')
+        if self.selected=='Add Device': #SN
+            self.getInput=Label(self.entry,text='*Enter Device SN:')
             self.getInput.pack()
             self.devSnEntry=Entry(self.entry)
             self.devSnEntry.pack()
-        elif self.selected=='Rename Device':
-            self.getInput=Label(self.entry,text='Rename device to:')
+        elif self.selected=='Rename Device': #Name
+            self.getInput=Label(self.entry,text='*Rename device to:')
             self.getInput.pack()
             self.renameEntry=Entry(self.entry)
             self.renameEntry.pack()   
-        elif self.selected=='Bulk Add Address':
-            self.getInput=Label(self.entry,text='Enter address:')
+        elif self.selected=='Bulk Add Address': #Address
+            self.getInput=Label(self.entry,text='*Enter address:')
             self.getInput.pack()
             self.addrEntry=Entry(self.entry)
             self.addrEntry.pack()
-        elif self.selected=='Add VLAN':
-            self.getID=Label(self.entry,text='ID:')
+        elif self.selected=='Add VLAN': #Vlan ID, name, subnet, appliance IP
+            self.getID=Label(self.entry,text='*ID:')
             self.getID.pack()
             self.idEnt=Entry(self.entry)
             self.idEnt.pack()
-            self.getName=Label(self.entry,text='Name:')
+            self.getName=Label(self.entry,text='*Name:')
             self.getName.pack()
             self.nameEnt=Entry(self.entry)
             self.nameEnt.pack()
-            self.getSubnet=Label(self.entry,text='Subnet:')
+            self.getSubnet=Label(self.entry,text='*Subnet:')
             self.getSubnet.pack()
             self.subEnt=Entry(self.entry)
             self.subEnt.pack()
-            self.getAppIp=Label(self.entry,text='Appliance IP:')
+            self.getAppIp=Label(self.entry,text='*Appliance IP:')
             self.getAppIp.pack()
             self.appEnt=Entry(self.entry)
             self.appEnt.pack()
-        elif self.selected=='Delete VLAN':
-            self.getInput=Label(self.entry,text='Enter VLAN ID:')
+        elif self.selected=='Delete VLAN': #Vlan ID
+            self.getInput=Label(self.entry,text='*Enter VLAN ID:')
             self.getInput.pack()
             self.vlanIdEntry=Entry(self.entry)
             self.vlanIdEntry.pack()
-        elif self.selected=='Update Device Port':
-            self.getId=Label(self.entry,text='Enter port ID:')
+        elif self.selected=='Update Device Port': #Port ID, type[access,trunk] *optional(VLAN ID)
+            self.getId=Label(self.entry,text='*Enter port ID:')
             self.getId.pack()
             self.idEnt=Entry(self.entry)
             self.idEnt.pack()
-            self.getType=Label(self.entry,text='Enter port type:')
+            self.getType=Label(self.entry,text='*Enter port type [access/trunk]:')
             self.getType.pack()
             self.typeEnt=Entry(self.entry)
             self.typeEnt.pack()
-            self.getVlan=Label(self.entry,text='Enter port VLAN:')
+            self.getVlan=Label(self.entry,text='Enter port VLAN ID (if access):')
             self.getVlan.pack()
             self.vlanEnt=Entry(self.entry)
             self.vlanEnt.pack()
         self.doneButton=Button(self.entry,text='Done',command=self.quitInput)
         self.doneButton.pack()     
-    def quitInput(self):
+    def quitInput(self): #on button click, takes the stored entries and calls method given the selected option
         self.selected=self.alter.get()
         if self.selected=='Add Device':
-            self.to_add=[]
-            self.to_add.append(str(self.devSnEntry.get()))
-            claimDevices(self.getNetID(),self.to_add)
-            self.popuDevNames()
-        elif self.selected=='Rename Device': #do NOT name devices the same name, this will make the program bug out
+            self.addDevice()
+        elif self.selected=='Rename Device': #do NOT name devices a name that exists currently, this will make the program bug out
             renameDevice(self.getDevSerial(),self.renameEntry.get())
             self.popuDevNames()
-        elif self.selected=='Update Device Port': #if you specify trunk you shouldn't be adding a VLAN!!!
-            if self.vlanEnt.get():
+        elif self.selected=='Update Device Port':
+            if self.vlanEnt.get(): #if changing to access port and want to change/Add associated VLAN
                 updateDevSwitchportVLAN(
                     self.getDevSerial(),self.idEnt.get(),
                     self.typeEnt.get(),self.vlanEnt.get())
-            else:
+            else: #if changing to trunk/if no VLAN specified and want to change other info
                 updateDevSwitchport(
                     self.getDevSerial(),self.idEnt.get(),
                     self.typeEnt.get()) 
@@ -160,16 +160,29 @@ class MerakiWizard(Frame):
         self.entry.destroy()
 
     ####ORG FUNCTIONS####
-    def getOrgList(self): #return the list of accessible orgs
-        self.the_orgs=[org['name'] for org in self.org_info]
-        return self.the_orgs
-    def getOrgID(self,*args): #get org ID given org name
+    def getOrgList(self): #return the list of accessible org names
+        self.org_list=sorted([org['name'] for org in self.org_info])
+        return self.org_list
+    def getOrgID(self,*args): #get org ID given org name, used to get all network names in org
         self.name=self.orgMenu.get(self.orgMenu.curselection())
         self.org_id=''
         for orgs in self.org_info:
             if orgs['name']==self.name:
                self.org_id+=orgs['id']
         return self.org_id
+    def getOrgNetIDs(self): #get list all network IDs in an org given org ID
+        self.net_ids=[]
+        for nets in orgNetInfo(self.getOrgID()):
+            if nets['organizationId']==self.getOrgID():
+                self.net_ids.append(nets['id'])
+        return self.net_ids
+    def getOrgNetNames(self): #get list of all network names given list of netID
+        self.net_names=sorted([net['name'] for net in orgNetInfo(self.getOrgID())])
+        return self.net_names
+    def popuNetNames(self,*args): #populates listbox on click per orgNetwork button, refreshes each new click
+        self.netMenu.delete(0,END)
+        for name in self.getOrgNetNames():
+            self.netMenu.insert(END,name)
 
     ####NETWORK FUNCTIONS####
     def getNetID(self,*args): #get net ID given net name
@@ -180,57 +193,28 @@ class MerakiWizard(Frame):
             if nets['name']==self.name:
                 self.curr_net+=(nets['id'])
         return self.curr_net
-    def getOrgNetIDs(self): #get list all network IDs in an org given org ID
-        self.net_info=orgNetInfo(self.getOrgID())
-        self.net_ids=[]
-        for nets in self.net_info:
-            if nets['organizationId']==self.getOrgID():
-                self.net_ids.append(nets['id'])
-        return self.net_ids
-    def getOrgNetNames(self): #get list of all network names given list of netID
-        self.net_info=orgNetInfo(self.getOrgID())
-        self.net_ids=self.getOrgNetIDs()
-        self.net_names=[]
-        for ident in self.net_ids:
-            for nets in self.net_info:
-                if nets['id']==ident:
-                    self.net_names.append(nets['name'])
-        return self.net_names
-    def popuNetNames(self,*args): #populates listbox on click per orgNetwork button, refreshes each new click
-        self.netMenu.delete(0,END)
-        for name in self.getOrgNetNames():
-            self.netMenu.insert(END,name)
-    def getNetDevices(self,*args): #given selected network name, return dictlist of all devices in the network
-        self.net_info=orgNetInfo(self.getOrgID())
-        self.name=self.netMenu.get(self.netMenu.curselection())
-        self.curr_net=''
-        for nets in self.net_info:
-            if nets['name']==self.name:
-                self.curr_net+=(nets['id'])
-        return deviceInfo(self.curr_net)
-    def getNetDevSerials(self,*args): #gets SN of all network devices
-        self.dev_info=self.getNetDevices()
-        self.serial=[]
-        for device in self.dev_info:
-            self.serial.append(device['serial'])
-        return self.serial
+    def getNetDevSerials(self): #gets SN of all network devices
+        self.net_serials=[device['serial'] for device in deviceInfo(self.getNetID())]
+        return self.net_serials
 
     ####DEVICE FUNCTIONS####
     def getDevNames(self): #gets list of device names in a network
-        self.the_devs=[dev['name'] for dev in self.getNetDevices()]
-        return self.the_devs
+        self.dev_names=sorted([dev['name'] for dev in deviceInfo(self.getNetID())])
+        return self.dev_names
     def popuDevNames(self,*args): #populates listbox on click of selecting network
         self.devMenu.delete(0,END)
         for name in self.getDevNames():
             self.devMenu.insert(END,name)
     def getDevSerial(self,*args): #gets the device SN given the listbox selection
-        self.net_devs=self.getNetDevices()
-        self.curr_dev=''
+        self.net_devs=deviceInfo(self.getNetID())
         self.name=self.devMenu.get(self.devMenu.curselection())
+        self.curr_dev=''
         for devs in self.net_devs:
             if devs['name']==self.name:
                 self.curr_dev+=devs['serial']
         return self.curr_dev
+    def devInfoClick(self,*args):
+        print(specDevInfo(self.getDevSerial()))
     def getDevicesExcel(self,*args): #Add devices given JLL .xls(x) file
         self.file_name=fd.askopenfilename(initialdir='/',title='Select A File',filetypes=(('.xls','*.xls'),('all files','.')))
         self.wb=xlrd.open_workbook(self.file_name)
@@ -240,7 +224,12 @@ class MerakiWizard(Frame):
             if self.sheet.cell_value(row,40)!='Approved-Cancelled':
                 self.meraki_devices.append(self.sheet.cell_value(row,24))
         return self.meraki_devices[3:]
-        #needs to filter out only meraki devices
+        ####needs to filter out only meraki devices in update####
+    def addDevice(self,*args): #adds a specific device given
+        self.to_add=[] #the SN passed into the API call is supposed to be a tuple
+        self.to_add.append(str(self.devSnEntry.get()))
+        claimDevices(self.getNetID(),self.to_add)
+        self.popuDevNames()
     def delDevices(self,*args): #deletes (1)selected device (2)all devices of selected network
         if self.devMenu.curselection():
             removeDevices(self.getNetID(),self.getDevSerial())
--- meraki_GUI_APIcall.py
+++ meraki_GUI_APIcall.py
@@ -15,15 +15,14 @@ def commandList():
         'Delete VLAN', #done
         'Swap MX Warm Spare', #done
         'Blink LED', #done
-        'Update Device Port' #done
         ]
     return commands
 
 ####GETTING INFORMATION####
 #API key and dashboard
 API_KEY=os.environ.get('Meraki_API')
-dashboard=meraki.DashboardAPI(API_KEY, output_log=False, print_console=False)
-# dashboard=meraki.DashboardAPI(API_KEY)
+# dashboard=meraki.DashboardAPI(API_KEY, output_log=False, print_console=False)
+dashboard=meraki.DashboardAPI(API_KEY)
 
 #getOrganizations() API call
 def orgInfo():
@@ -34,6 +33,10 @@ def orgInfo():
 def orgNetInfo(org_id):
     getOrgNet=dashboard.organizations.getOrganizationNetworks(org_id, total_pages='all')
     return getOrgNet
+
+#getNetwork() API call
+def netInfo(net_id):
+    dashboard.networks.getNetwork(net_id)
 
 #getNetworkDevices() API call
 def deviceInfo(net_id):
@@ -70,6 +73,10 @@ def removeDevices(net_id,serial):
 def renameDevice(serial,dev_name):
     dashboard.devices.updateDevice(serial,name=dev_name)
 
+#bulk adds address when called for each SN in network
+def setAddress(serials,address):
+    dashboard.devices.updateDevice(serials,address=address)
+
 #swapNetworkApplianceWamSpare() API call
 def swapWarmSpare(net_id):
     dashboard.appliance.swapNetworkApplianceWarmSpare(net_id)
@@ -77,46 +84,32 @@ def swapWarmSpare(net_id):
 #createVLAN() API call
 def createVLAN(net_id,id,name,subnet,ip):
     dashboard.appliance.createNetworkApplianceVlan(
-        net_id,id,name,subnet,ip)
+        net_id,id,name,subnet,ip
+        )
 
 #blinkDeviceLeds() API call
 def blinkDevice(serial):
     dashboard.devices.blinkDeviceLeds(serial, duration=20, period=160, duty=50)
 
-def setAddress(serials,address): #bulk change address
-    dashboard.devices.updateDevice(serials,address=address)
-
+#if no VLAN input aka trunk port update or access port with unchanged VLAN ID
 def updateDevSwitchport(serial,id,port_type):
     dashboard.switch.updateDeviceSwitchPort(
         serial, id, 
         type=port_type,
     )
 
+#if there is a VLAN input aka access port update
 def updateDevSwitchportVLAN(serial,id,port_type,vlan):
     dashboard.switch.updateDeviceSwitchPort(
         serial, id, 
         type=port_type, 
-        vlan=vlan,
+        vlan=vlan
     )
 
 #updateNetworkApplianceVlan() API call
-# def updateVLAN():
-#     response = dashboard.appliance.updateNetworkApplianceVlan(
-#     network_id, vlan_id, 
-#     name='My VLAN', 
-#     subnet='192.168.1.0/24', 
-#     applianceIp='192.168.1.2', 
-#     groupPolicyId='101', 
-#     dhcpHandling='Run a DHCP server', 
-#     dhcpLeaseTime='1 day', 
-#     dhcpBootOptionsEnabled=False, 
-#     dhcpBootNextServer=None, 
-#     dhcpBootFilename=None, 
-#     fixedIpAssignments={'22:33:44:55:66:77': {'ip': '1.2.3.4', 'name': 'Some client name'}}, 
-#     reservedIpRanges=[{'start': '192.168.1.0', 'end': '192.168.1.1', 'comment': 'A reserved IP range'}], 
-#     dnsNameservers='google_dns', 
-#     dhcpOptions=[{'code': 5, 'type': 'text', 'value': 'five'}]
-# )
+def updateVLAN(vlan_id,ip):
+    dashboard.appliance.updateNetworkApplianceVlan(vlan_id,ip)
 
+#deleteNetworkApplianceVlan() API call
 def removeVLAN(net_id,vlan_id):
     dashboard.appliance.deleteNetworkApplianceVlan(net_id, vlan_id)
